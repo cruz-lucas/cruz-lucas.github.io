23ac2ccd5fc78cacc420c84408b34964ee7127cb
Lucas Cruz <lucasdearaujocruz@gmail.com>
2024-08-05 15:00:08 -0300

    feat: add gtag

--- src/components/RL/Grid/index.tsx
+++ src/components/RL/Grid/index.tsx
@@ -1,3 +1,4 @@
 export { default as PlayableGrid } from './PlayableGrid';
 export { default as AnimationGrid } from './AnimationGrid';
 export { default as StaticGrid } from './StaticGrid';
+export { default as AnimationQGrid } from './AnimationQGrid';
